9a690a3916c03933892ad9b190e353d010e02aa5
Cai Williamson <caiwilliamson@users.noreply.github.com>
2022-06-26 12:22:51 +0100

    Disable Homebrew analytics

--- dotfiles/config/fish/config.fish
+++ dotfiles/config/fish/config.fish
@@ -3,6 +3,7 @@ set -U fish_greeting
 
 # Environment variables
 set -gx RUBY_CONFIGURE_OPTS "--with-openssl-dir=$(brew --prefix openssl@1.1)"
+set -gx HOMEBREW_NO_ANALYTICS 1
 set -gx EDITOR vim
 
 # asdf
