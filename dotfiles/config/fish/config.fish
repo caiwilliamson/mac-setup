babea66c142145620a4d0562039376c12cdd2cb5
Cai Williamson <caiwilliamson@users.noreply.github.com>
2022-06-29 13:53:07 +0100

    Simplify config.fish

--- dotfiles/config/fish/config.fish
+++ dotfiles/config/fish/config.fish
@@ -1,25 +1,24 @@
-# Disable greeting.
-set -U fish_greeting
+# Locate Homebrew installation.
+[ -d /opt/homebrew ]
+  and set -gx HOMEBREW_PREFIX /opt/homebrew # Apple Silicon.
+  or  set -gx HOMEBREW_PREFIX /usr/local    # Intel.
 
-# Set HOMEBREW_PREFIX based on processor architecture.
-switch (uname -m)
-  case x86_64; set HOMEBREW_PREFIX "/usr/local" # Intel.
-  case arm64; set HOMEBREW_PREFIX "/opt/homebrew" # Apple Silicon.
-  case '*'; echo "Processor architecture not recognised" && exit 1
-end
+# Add brew to PATH and set some environment variables.
+eval ($HOMEBREW_PREFIX/bin/brew shellenv)
 
+set -gx XDG_CONFIG_HOME $HOME/.config
+set -gx XDG_CACHE_HOME  $HOME/.cache
+set -gx XDG_DATA_HOME   $HOME/.local/share
+
+set -U  fish_greeting # Disable greeting.
 set -gx HOMEBREW_NO_ANALYTICS 1
 set -gx EDITOR vim
 # https://github.com/rbenv/ruby-build/wiki#suggested-build-environment
-set -gx RUBY_CONFIGURE_OPTS "--with-openssl-dir=$($HOMEBREW_PREFIX/bin/brew --prefix openssl@1.1)"
+set -gx RUBY_CONFIGURE_OPTS --with-openssl-dir=(brew --prefix openssl@1.1)
 
-# Prepend asdf directories to PATH.
-source $HOMEBREW_PREFIX/opt/asdf/libexec/asdf.fish
+# Add asdf to PATH.
+source (brew --prefix asdf)/libexec/asdf.fish
 
-# Prepend Homebrew directories to PATH on Apple Silicon Macs only. On Intel
-# machines Homebrew uses /usr/local which is already added to PATH.
-[ (uname -m) = arm64 ] && eval "$($HOMEBREW_PREFIX/bin/brew shellenv)"
-
-# Append subl and smerge CLI tool directories to PATH.
+# Add subl and smerge to PATH.
 fish_add_path --append /Applications/Sublime\ Text.app/Contents/SharedSupport/bin/
 fish_add_path --append /Applications/Sublime\ Merge.app/Contents/SharedSupport/bin/
